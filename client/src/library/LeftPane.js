ef86dbb920f73e127b27c737058c78462a19723b
liliana Quinley <lilicuellar99@gmail.com>
2019-02-02 14:13:26 -0700

    Added card to the Picture Left Panel

--- client/src/library/LeftPane.js
+++ client/src/library/LeftPane.js
@@ -1,29 +1,72 @@
 import React, { Component } from 'react';
-
 import Paper from "@material-ui/core/Paper";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Picture from "../Assets/Images/Liliana_Quinley.jpg"
 
-import picture from '../Assets/Images/Liliana_Quinley.jpg'
+
+function LeftPane(props) {
+  
+  return (
+
+    < Paper style = {styles.Paper}>
+    Left Pane
 
 
-
-export class LeftPane extends Component {
-  render() {
-    return (
-      <div>
-         <Paper style = {styles.Paper}>
-         
-            <img src = {picture} alt = "picture"/>
-         
-            Left Pane
-        </Paper>
-        </div>
-    )
-  }
+    <Card >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+            
+          height=""
+          image= { Picture }
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            my website
+          </Typography>
+          <Typography component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+    </Paper>
+  );
 }
+
 const styles = {
     Paper : {padding: 20, marginTop: 10, marginBottom: 10 },
-    picture : {width : 250, height : 250}
-}
+    
+    card: {
+        width: 151
+      },
+      media: {
+        objectFit: 'cover',
+      },
+    };
+    
+    LeftPane.propTypes = {
+        classes: PropTypes.object.isRequired,
+      };
 
 export default LeftPane
 
